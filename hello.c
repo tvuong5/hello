2c500f555b9164bdbc2644317ee6485d27a76076
Trung <tvuong5@huskers.unl.edu>
2024-08-29 13:14:03 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,5 +1,16 @@
 /*
-* Print Name code
-*
+* Author: Trung Vuong
+*   Date: 08/29/2024
+*   Hack 1.0
+* Print Name and major
 */
-printf("Trung\n");
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Trung Vuong!\n");
+  printf("My major is Computer Engineering\n");
+
+  return 0;
+}
